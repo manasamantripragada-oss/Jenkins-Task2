b4454a651f3129c261ab4d65c103749267505ee8
manasamantripragada-oss <manasa.mantripragada@gmail.com>
2025-10-19 12:07:21 +0530

    Windows execution

---
+++ build.bat
@@ -0,0 +1,12 @@
+@echo off
+echo ===========================
+echo Building Jenkins Project...
+echo ===========================
+echo Current Directory: %CD%
+
+rem Example build step
+dir
+
+echo ===========================
+echo Build Completed Successfully!
+echo ===========================
